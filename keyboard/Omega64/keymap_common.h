8744c9d101f8485f7bde46e11672655564d804fb
wangssp <93472706+wangssp@users.noreply.github.com>
2021-11-06 16:36:11 +0800

    Update keymap_common.h

--- keyboard/Omega64/keymap_common.h
+++ keyboard/Omega64/keymap_common.h
@@ -47,33 +47,3 @@ extern const action_t fn_actions[];
 	{ K40,    K41,    K42,    K43,    K44,    K45,    K46,    K47,    K48,    K49,    K4A,    K4B,    KC_NO,  K4D,    K4E,    K4F,    K4G,    KC_NO}, \
 	{ K50,    K51,    K52,    KC_NO,  KC_NO,    K55,  KC_NO,  KC_NO,  K58,    K59,    K5A,    KC_NO,  K5C,    K5D,    K5E,    K5F,    K5F,    K5H }  \
 }
-
-/* ANSI variant. No extra keys for ISO */
-#define KEYMAP_ANSI( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A  \
-) KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A  \
-)
-
-
-#define KEYMAP_HHKB( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A  \
-) KEYMAP( \
-	K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, K0D, \
-	K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, \
-	K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C,      \
-	K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, \
-	K40, K41, K42, K43,           K46, K47, K48, K49, K4A  \
-)
